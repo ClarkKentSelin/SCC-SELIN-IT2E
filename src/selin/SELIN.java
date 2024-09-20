a0198720a9fc37e6d11a681247aaa49c83068a0e
ClarkKentSelin <aionoclark@gmail.com>
2024-09-20 14:48:04 +0800

    FOC

--- src/selin/SELIN.java
+++ src/selin/SELIN.java
@@ -1,21 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package selin;
 
-/**
- *
- * @author Admin
- */
+import java.util.Scanner;
+
 public class SELIN {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner sc = new Scanner(System.in);
+        Grade grd = new Grade();
+        Sale sl = new Sale();
+        Product pr = new Product();
+        Account ac = new Account();
+        
+        String Confirm;
+        
+        do{
+            System.out.print("\nSelect Program to run:"
+                + "\n1. Products"
+                + "\n2. Sales"
+                + "\n3. Grades"
+                + "\n4. Accounts"
+                + "\nEnter selection: ");
+        int select = sc.nextInt();
+                
+        switch(select){
+            case 1:
+                pr.getProduct();
+                break;
+            case 2:
+                sl.getSales();
+                break;
+            case 3:
+                grd.getGrades();
+                break;
+            case 4:
+                ac.getAccounts();
+                break;
+        }   
+        
+            System.out.print("Make another transaction? (y/n): ");
+            Confirm = sc.next();
+            
+        } while(Confirm.equals("y"));
+        
+        System.out.println("\nThank you for using the system!");
     }
-    
 }
+    
